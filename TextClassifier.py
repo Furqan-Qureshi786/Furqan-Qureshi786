a128968fd811d473096a3a4a392d066e71e47df3
FURQAN QURESHI <141150633+Furqan-Qureshi786@users.noreply.github.com>
2024-10-05 13:34:51 +0500

    Update TextClassifier.py

--- TextClassifier.py
+++ TextClassifier.py
@@ -5,18 +5,18 @@ import numpy as np
 import pandas as pd
 
 # Load the model
-@st.cache_resource
 def load_model():
     embedding = "https://tfhub.dev/google/nnlm-en-dim50/2"
-    hub_layer = hub.KerasLayer(embedding, dtype=tf.string, trainable=True)
+    hub_layer = hub.KerasLayer(embedding, input_shape=[], dtype=tf.string, trainable=True)
 
-    model = tf.keras.Sequential()
-    model.add(hub_layer)  # Directly add the hub layer
-    model.add(tf.keras.layers.Dense(16, activation='relu'))
-    model.add(tf.keras.layers.Dropout(0.4))
-    model.add(tf.keras.layers.Dense(16, activation='relu'))
-    model.add(tf.keras.layers.Dropout(0.4))
-    model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+    model = tf.keras.Sequential([
+        hub_layer,  # Add hub layer directly
+        tf.keras.layers.Dense(16, activation='relu'),
+        tf.keras.layers.Dropout(0.4),
+        tf.keras.layers.Dense(16, activation='relu'),
+        tf.keras.layers.Dropout(0.4),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
     
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
                   loss=tf.keras.losses.BinaryCrossentropy(),
@@ -24,7 +24,7 @@ def load_model():
     
     return model
 
-model = load_model()
+model = load_model()  # Load the model without caching
 
 # Load and preprocess your dataset
 train_data = pd.read_csv('https://raw.githubusercontent.com/Furqan-Qureshi786/TextClassifier/main/wine-reviews.csv')
@@ -63,4 +63,3 @@ if st.button('Predict'):
             st.text(f'Probability of being High Quality: {pred_prob_value:.2f}')
         except Exception as e:
             st.error(f'Error during prediction: {e}')
-
